93834bbef353a150e2f24da3ae384da05f66c3c1
AlphaNecron <work@xwork.space>
2022-02-27 17:53:49 +0700

    initial commit, experimental

---
+++ js/evo.js
@@ -0,0 +1,120 @@
+const powerOperations = {
+    "Shutdown": "&#xe9c0;",
+    "Restart": "&#xe9c4;",
+    "Hibernate": "&#xe9e2;",
+    "Suspend": "&#xe9a3;"
+};
+
+let timeWidgetMode = 1;
+let currentUser;
+let currentSession;
+
+function update_time() {
+    $("dt").innerText = timeWidgetMode === 0 ? new Date().toLocaleDateString() : new Date().toLocaleTimeString();
+}
+
+this.addEventListener("load", () => {
+    $("dt").addEventListener("click", e => {
+        timeWidgetMode = timeWidgetMode === 0 ? 1 : 0;
+        update_time();
+    });
+    document.querySelectorAll("[dropdown]").forEach(btn => {
+        const dropdown = btn.attributes.dropdown.value;
+        btn.addEventListener("click", () => $(dropdown).classList.toggle("show"));
+    });
+    update_time();
+    setInterval(() => update_time(), 1000);
+    Object.entries(powerOperations).filter(([o, _]) => lightdm[`can_${o.toLowerCase()}`]).forEach(([o, i]) => $("powermenu").appendChild(make_menu_item(`<p class="icon" style="display: inline">${i}</p> ${o}`, () => handle_power_operation(o.toLowerCase()))));
+    lightdm.users.forEach(usr => $("user-dropdown").appendChild(make_menu_item(`<img src="${usr.image}" class="sm-avatar"></img>${usr.display_name}`, () => set_user(usr))));
+    lightdm.sessions.forEach(s => $("session-list").appendChild(make_menu_item(s.name, () => set_session(s))));
+    $("hostname").innerText = lightdm.hostname;
+    set_user(lightdm.select_user);
+});
+
+function make_menu_item(inner, click) {
+    const d = document.createElement("div");
+    d.addEventListener("click", click);
+    d.innerHTML = inner;
+    return d;
+}
+
+function toast(title, isErr) {
+    const toast = document.createElement("div");
+    if (isErr) toast.className = "err";
+    toast.innerHTML = title;
+    toast.addEventListener("click", () => $("toast-container").removeChild(toast))
+    $("toast-container").appendChild(toast);
+    setTimeout(() => {
+        if ($("toast-container").contains(toast)) {
+            [1, 0.75, 0.5, 0.25].forEach(x => setTimeout(() => toast.style.opacity = x, 25 / x));
+            setTimeout(() => $("toast-container").removeChild(toast), 100);
+        }
+    }, 2000);
+}
+
+function authentication_complete() {
+    if (lightdm.is_authenticated)
+        lightdm.start_session_sync(currentSession)
+    else {
+        if (lightdm._username){
+            lightdm.cancel_authentication();
+        }
+        if (currentUser) lightdm.start_authentication(currentUser.name);
+        show_error("Wrong password!");
+    }
+}
+
+function show_error(err) {
+    toast(err, true);
+}
+
+function show_prompt(text, type) {
+    if (type === "password") {
+        $("password-box").innerText = "";
+        $("password-box").focus();
+    } else toast(text, false);
+}
+
+function show_message(msg) {
+    toast(msg, false);
+}
+
+function handle_power_operation(op) {
+    if (lightdm[`can_${op}`]) {
+        lightdm[op]();
+    }
+}
+
+function provide_secret() {
+    password = $("password-box").value || null;
+    if (password !== null)
+        lightdm.respond(password);
+}
+
+window.addEventListener("click", e => {
+    if (!e.target.attributes.dropdown)
+        Array.from(document.getElementsByClassName("drop-container")).filter(drop => drop.classList.contains("show")).forEach(drop =>
+            drop.classList.remove("show")
+        );
+});
+
+function set_session(session) {
+    currentSession = session;
+    $("session").innerText = session.name;
+}
+
+function set_user(user) {
+    currentUser = user;
+    if (lightdm._username){
+        lightdm.cancel_authentication();
+    }
+    if (user) lightdm.start_authentication(user.name);
+    set_session(lightdm.sessions.find(s => s.key === currentUser.session) || currentSession || lightdm.default_session);
+    $("avatar").src = user.image;
+    $("name").textContent = user.display_name;
+    $("password-box").focus();
+}
+
+function $(id) {
+    return document.getElementById(id);
+}
---
+++ js/mock.js
@@ -0,0 +1,149 @@
+// lightdm-gab-gradient
+
+if (!('lightdm' in window)) {
+    document.title = "Evo - Live Demo";
+    window.lightdm = {};
+    lightdm.hostname ="evo";
+    lightdm.sessions = [
+        {
+            key: "bspwm",
+            name: "BSPWM",
+            comment: "no comment"
+        },
+        {
+            key: "openbox",
+            name: "Openbox",
+            comment: "no comment"
+        },
+        {
+            key: "plasma",
+            name: "KDE Plasma",
+            comment: "no comment"
+        }
+    ];
+    lightdm.default_session = lightdm.sessions[0];
+    lightdm.authentication_user = null;
+    lightdm.is_authenticated = false;
+    lightdm.can_suspend = true;
+    lightdm.can_hibernate = true;
+    lightdm.can_restart = true;
+    lightdm.can_shutdown = true;
+    lightdm.users = [
+        {
+            name: "necron",
+            real_name: "Nguyen Thanh Quang",
+            display_name: "AlphaNecron",
+            image: "https://avatars.githubusercontent.com/u/57827456?v=4",
+            language: "en_US",
+            layout: null,
+            session: "bspwm",
+            logged_in: false
+        },
+        {
+            name: "venus",
+            real_name: "Nguyen Van Thanh Lam",
+            display_name: "BayuLewis",
+            image: "https://avatars.githubusercontent.com/u/68362244?v=4",
+            language: "en_US",
+            layout: null,
+            session: "openbox",
+            logged_in: false
+        }
+    ];
+    lightdm.select_user = lightdm.users[0];
+    lightdm.num_users = lightdm.users.length;
+    lightdm.timed_login_delay = 0;
+    lightdm.timed_login_user =
+        lightdm.timed_login_delay > 0 ? lightdm.users[0] : null;
+    lightdm.get_string_property = () => {};
+    lightdm.get_integer_property = () => {};
+    lightdm.get_boolean_property = () => {};
+    lightdm.cancel_timed_login = function () {
+        _lightdm_mock_check_argument_length(arguments, 0);
+        lightdm._timed_login_cancelled= true;
+    };
+
+    lightdm.respond = function (secret) {
+        if (typeof lightdm._username == 'undefined' || !lightdm._username) {
+            show_error("start_authentication must be called first!");
+        }
+        _lightdm_mock_check_argument_length(arguments, 1);
+        user = _lightdm_mock_get_user(lightdm.username);
+        if (!user && secret == lightdm._username) {
+            lightdm.is_authenticated = true;
+            lightdm.authentication_user = user;
+        } else {
+            lightdm.is_authenticated = false;
+            lightdm.authentication_user = null;
+            lightdm._username = null;
+        }
+        authentication_complete();
+    };
+
+    lightdm.start_authentication = function (username) {
+        _lightdm_mock_check_argument_length(arguments, 1);
+        if (lightdm._username)
+            show_error("Already authenticating!");
+        user = _lightdm_mock_get_user(username);
+        if (!user)
+            show_error(username + " is an invalid user");
+        lightdm._username = username;
+    };
+
+    lightdm.cancel_authentication = function() {
+        _lightdm_mock_check_argument_length(arguments, 0);
+
+        if (!lightdm._username)
+            show_error("Not authenticating");
+        lightdm._username = null;
+    };
+
+    lightdm.suspend = () =>
+        show_message("Suspending...");
+
+    lightdm.hibernate = () =>
+        show_message("Hibernating...");
+
+    lightdm.restart = () =>
+        show_message("Restarting...");
+
+    lightdm.shutdown = () =>
+        show_message("Shutting down...");
+
+    lightdm.start_session_sync = function (session) {
+        _lightdm_mock_check_argument_length(arguments, 1);
+        if (!lightdm.is_authenticated)
+            show_error("The system is not authenticated")
+        else if (user !== lightdm.authentication_user)
+            show_error("This user is not authenticated")
+        else if (!session) show_error("Invalid session!");
+        else show_message("Logged in successfully!");
+        document.location.reload(true);
+    };
+
+    if (lightdm.timed_login_delay > 0) {
+        setTimeout(
+            () => {
+                if (!lightdm._timed_login_cancelled()) timed_login();
+            },
+            lightdm.timed_login_delay
+        );
+    }
+}
+
+const _lightdm_mock_check_argument_length = (args, length) => {
+    if (args.length != length) {
+        show_prompt("Incorrect number of arguments in function call");
+    }
+}
+
+const _lightdm_mock_get_user = username => {
+    let usr = null, i;
+    for (i = 0; i < lightdm.users.length; ++i) {
+        if (lightdm.users[i].name == username) {
+            usr = lightdm.users[i];
+            break;
+        }
+    }
+    return usr;
+}
